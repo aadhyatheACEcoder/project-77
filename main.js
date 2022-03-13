3756451ec1b95a00357cf6bd3a7703e7d352d043
Aadhya J <96498686+aadhyatheACEcoder@users.noreply.github.com>
2022-03-13 11:01:19 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var firstname="Aadhya"
+console.log(firstname)
+var lastname="Jhalani"
+console.log(lastname)
+var fullname=firstname.concat( lastname)
+console.log(fullname)
+var numberone="9"
+console.log(numberone)
+var numbertwo="12"
+console.log(numbertwo)
+var add=9 + 12
+console.log(add)
+var subtract=numbertwo-numberone
+console.log(subtract)
+var multiply=numberone*numbertwo
+console.log(multiply)
